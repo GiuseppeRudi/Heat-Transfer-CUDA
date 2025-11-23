492a5c0f19bbc2e0b0354bf0632d662ff9390c10
GiuseppeRudi <rudigiuseppe1@gmail.com>
2025-11-23 09:13:30 +0100

    refactor to adapt this repository for linux and gtx 980

---
+++ include/cuda/kernel_config.cuh
@@ -0,0 +1,16 @@
+//
+// Created by giu20 on 19/11/2025.
+//
+
+#ifndef HEAT_TRANSFER_CUDA_KERNEL_CONFIG_CUH
+#define HEAT_TRANSFER_CUDA_KERNEL_CONFIG_CUH
+
+
+
+class kernel_config {
+
+};
+
+
+
+#endif //HEAT_TRANSFER_CUDA_KERNEL_CONFIG_CUH
